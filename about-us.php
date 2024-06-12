9f22545f5abee35157359b2d40105edf5638f674
nitin2001581402 <141765587+nitin2001581402@users.noreply.github.com>
2024-06-12 17:52:19 +0530

    Add files via upload
    It has a user-friendly interface and is built so that customers may utilize a website to purchase a wide range of mouthwatering foods.

---
+++ about-us.php
@@ -0,0 +1,74 @@
+<?php
+session_start();
+include('includes/header.php');
+
+?>
+
+<div class="banner-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3 class="banner-heading">About Us</h3>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="mb-3">
+                    <h4 class="main-heading">About <span class="text-orange">Lemon Tree Restaurant</span></h4>
+                    <div class="underline"></div>
+                </div>
+                <p>
+                    "In case you’re searching for top-notch food, ‘Lemon Tree’ is unquestionably one of the spots to go in Mysuru. The adaptable menu flaunts some imaginative food,"!
+                </p>    
+                <p>
+                    "To create an environment where absolute guest satisfaction is our highest priority"!
+                </p>    
+            </div>
+           
+        </div>
+    </div>
+</div>
+
+<?php include('pages/about-us.php'); ?>
+
+<div class="py-5 bg-f2f2f2">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="mb-3">
+                    <h4>Our Mission</h4>
+                    <div class="underline"></div>
+                </div>
+                <p>
+                "To create an environment where absolute guest satisfaction is our highest priority"!!
+                    <br>
+                    <br>
+                    In Here, It’s Always Friday. Each of us bases every decision on what is best for our Guest. We operate as One brand with uncompromising Standards of Excellence around our Credo, our guiding principles, and our Culture of Fun. We deliver on our financial commitments, but never at the expense of the safety and security of our Guest, Team Members, and the brand..!
+                </p>    
+                   
+            </div>
+            <div class="col-md-6">
+                <div class="mb-3">
+                    <h4>Our Vision</h4>
+                    <div class="underline"></div>
+                </div>
+                <p>
+                Through a shared commitment to excellence, we are dedicated to the uncompromising quality of our food, service, people and profit, while taking exceptional care of our guests and staff. We will continuously strive to surpass our own accomplishments and be recognized as a leader in our industry..!
+                    <br>
+                    <br>
+
+        
+                </p>    
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include('includes/footer.php');
+?>
